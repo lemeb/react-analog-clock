cafda29d3fd30883f1991c1138514c2e2b1f9c41
Léopold Mebazaa <lemebfr@gmail.com>
2017-10-27 15:21:20 +0200

    Changed to radio style, with hidden ticks and centered time

--- demo/index.js
+++ demo/index.js
@@ -3,19 +3,14 @@ import ReactDOM from 'react-dom';
 import AnalogClock, { Themes } from '../src/index';
 
 const WIDTH = 200;
-const GMTOFFSET= "-5.5";
+const GMTOFFSET= "+2";
 
 const Component = (
     <div>
         <span><AnalogClock width={WIDTH} theme={Themes.light} /></span>
-        <span><AnalogClock width={WIDTH} theme={Themes.dark} /></span>
-        <span><AnalogClock width={WIDTH} theme={Themes.aqua} /></span>
-        <span><AnalogClock width={WIDTH} theme={Themes.lime} /></span>
-        <span><AnalogClock width={WIDTH} theme={Themes.sherbert} /></span>
-        <span><AnalogClock width={WIDTH} theme={Themes.navy} /></span>
-
-        <h2>With GMT Offset -5.5:</h2>
-        <span><AnalogClock width={WIDTH} theme={Themes.light} gmtOffset={GMTOFFSET} /></span>
+        <span><AnalogClock width={WIDTH} theme={Themes.radio} /></span>
+        <h2>With GMT Offset {GMTOFFSET}:</h2>
+        <span><AnalogClock width={WIDTH} theme={Themes.radio} gmtOffset={GMTOFFSET} /></span>
     </div>
 );
 
